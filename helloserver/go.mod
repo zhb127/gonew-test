86f32b275f412a61c33fe9e80adbdda40d0b914f
bocai <1224680008@qq.com>
2023-08-11 15:22:11 +0800

    feat: init

---
+++ helloserver/go.mod
@@ -0,0 +1,4 @@
+module github.com/zhb127/gonew-test/helloserver
+
+go 1.19
+
---
+++ helloserver/scripts/init-local-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# init-local-dev
+
+echo "start local db..."
+
+echo "start local redis..."
+
+echo "The local development environment is initialized."
